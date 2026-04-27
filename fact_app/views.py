198cc8414d3741b07183ebe5758c713ce30a2d23
Madou DIAKAYETEBA <madoudiakayeteba@gmail.com>
2026-04-27 23:41:11 +0200

    feat: (jour3) add Customer, Article, and Invoice models with business logic

--- fact_app/views.py
+++ fact_app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import  *
+from django.views import View
 
 # Create your views here.
-def home(request, *args, **kwargs):
-    return render(request, 'base.html')
+class HomeView(TemplateView):
+    """ Main page view """
+    template_name = 'index.html'
+    
+    Invoice = Invoice.objects.select_related('customer').all()
+
+    context = {
+        'invoices': Invoice,    
+    }
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.context)
+    
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.context)    
+    
