66648cc2995419b731ac2aae34d42a97d7dd1ac5
Madou DIAKAYETEBA <madoudiakayeteba@gmail.com>
2026-04-29 23:52:41 +0200

    feat: mise en place des messages d'erreurs lors de l'enregistrement des customers

--- fact_app/views.py
+++ fact_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import  *
 from django.views import View
+from django.contrib import messages
+
+
+
+
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -30,4 +35,28 @@ class HomeView(TemplateView):
         
         def post(self, request, *args, **kwargs):
             # Logic to handle form submission for adding a new customer
+            data= {
+                'name': request.POST.get('name'),
+                'email': request.POST.get('email'),
+                'phone': request.POST.get('phone'),
+                'address': request.POST.get('address'),
+                'sex': request.POST.get('sex'),
+                'age': request.POST.get('age'),
+                'city': request.POST.get('city'),
+                'zip_code': request.POST.get('zip'),    
+                'saved_by': request.user,  # Assuming the user is authenticated
+            }
+            
+            try:
+                  created= Customer.objects.create(**data)
+
+                  if created:
+                    messages.success(request, 'Customer registered successfully!')   
+
+                  else:
+                         messages.error(request, 'Sorry,. Please try again the sent data is not valid!')
+
+                except Exception as e:
+                    messages.error(request, 'Sorry our system is detecting the following issues: {e}')
+
             return render(request, self.template_name,)
